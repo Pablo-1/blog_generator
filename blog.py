b1ab7c17e39f3d12490a53d748a99360bfa95d2b
Pawel Gonczarewicz <p.gonczarewicz@gmail.com>
2023-04-18 17:19:06 +0100

    Blogpost generator finished.

--- blog.py
+++ blog.py
@@ -14,10 +14,10 @@ title_box_lab = tk.Label(root, text="Type the subject of your post in the box be
 title_box_lab.pack(pady=10)
 
 # Subject box
-textbox = tk.Text(root, font=('Arial', 15), height=2)
+textbox = tk.Text(root, font=('Arial', 15), height=1)
 textbox.pack(padx=20)
 
-# Generate blog function
+# Generate blog button
 def generate_blogpost():
     # Get the subject from the textbox from first character to last -1 which is new line character 
     subject = textbox.get("1.0", "end-1c")
@@ -36,11 +36,9 @@ def generate_blogpost():
         prompt=f"Write a blog post on {subject}.",
         # Argument on the scale of 0.0 to 1.0, controls creativity and risktaking 
         temperature=0.5,
-        #Number of tokens
         max_tokens=1024,
         # Number of responses to generate
         n=1,
-        # Argument stops generating response if the condition is met, set to None
         stop=None,
         # Number of seconds for response to be generated before its terminated
         timeout=10,
@@ -52,9 +50,9 @@ def generate_blogpost():
     # Insert the blog post into the blogpost box
     blogpost_box.configure(wrap='word')
     blogpost_box.delete("1.0", "end")
-    blogpost_box.insert("1.0", blogpost)
+    # Inserting response, blogpost.strip() gets rid of empty lines at the beginning being put there by the API
+    blogpost_box.insert("1.0", blogpost.strip())
 
-# Generate blog button
 gen_button = tk.Button(root, text="Generate", font=('Arial', 15), command=generate_blogpost)
 gen_button.pack(pady=10)
 
